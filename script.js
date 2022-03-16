5dbb7586c6106cbb72bfd7292ef9b9b7b9080dd9
sanu-a <96769411+sanu-a@users.noreply.github.com>
2022-03-17 01:52:15 +0530

    complete

--- script.js
+++ script.js
@@ -3,62 +3,59 @@ let gamePoints = 0;
 let canPlay = true;
 
 function updateGamePoints() {
-    const gamePointsElement = document.getElementById("gamePoints");
-    gamePointsElement.innerHTML = "Game Point is " + gamePoints;
+  const gamePointsElement = document.getElementById("gamePoints");
+  gamePointsElement.innerHTML = "Game Point is " + gamePoints;
 }
 
 function refresh() {
-    const refreshElement = document.getElementById("refresh");
-    refreshElement.addEventListener("click", () => {
-        location.reload();
-    });
+  location.reload();
 }
 
 function addGrid() {
-    const app = document.getElementById("app");
-    for (let i = 0; i < 9; i++) {
-        const row = document.createElement("div");
-        for (let j = 0; j < 9; j++) {
-            const index = i * 9 + j;
-            const col = document.createElement("div");
-            col.setAttribute("index", index);
+  const app = document.getElementById("app");
+  for (let i = 0; i < 9; i++) {
+    const row = document.createElement("div");
+    for (let j = 0; j < 9; j++) {
+      const index = i * 9 + j;
+      const col = document.createElement("div");
+      col.setAttribute("index", index);
 
-            col.style.border = "1px solid black";
-            col.style.display = "inline-block";
-            col.style.height = "60px";
-            col.style.width = "60px";
-            col.style.textAlign = "center";
-            col.style.verticalAlign = "middle";
+      col.style.border = "1px solid black";
+      col.style.display = "inline-block";
+      col.style.height = "60px";
+      col.style.width = "60px";
+      col.style.textAlign = "center";
+      col.style.verticalAlign = "middle";
 
-            col.addEventListener("click", () => {
-                if (canPlay) {
-                    if (bombs.includes(index)) {
-                        col.style.background = "red";
-                        setTimeout(() => {
-                            alert("Game over");
-                        }, 500);
-                        canPlay = false;
-                    } else {
-                        col.style.background = "green";
-                        gamePoints++;
-                        updateGamePoints();
-                    }
-                }
-            });
-
-            row.appendChild(col);
+      col.addEventListener("click", () => {
+        if (canPlay) {
+          if (bombs.includes(index)) {
+            col.style.background = "red";
+            setTimeout(() => {
+              alert("Game over");
+            }, 500);
+            canPlay = false;
+          } else if (!col.style.background) {
+            col.style.background = "green";
+            gamePoints++;
+            updateGamePoints();
+          }
         }
-        app.appendChild(row);
+      });
+
+      row.appendChild(col);
     }
+    app.appendChild(row);
+  }
 }
 
 function generateBombs() {
-    while (bombs.length < 10) {
-        const randomNumber = Math.floor(Math.random() * 100);
-        if (randomNumber < 81 && !bombs.includes(randomNumber)) {
-            bombs.push(randomNumber);
-        }
+  while (bombs.length < 10) {
+    const randomNumber = Math.floor(Math.random() * 100);
+    if (randomNumber < 81 && !bombs.includes(randomNumber)) {
+      bombs.push(randomNumber);
     }
+  }
 }
 
 addGrid();
